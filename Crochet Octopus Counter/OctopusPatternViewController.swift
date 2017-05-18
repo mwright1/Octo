1ca804242904b4392d0f8f92e97dca59c4bb9853
Mona Wright <mlpw@uw.edu>
2017-05-18 16:38:49 -0700

    Added code for repeating rounds and fixed truncated label text.

--- Crochet Octopus Counter/OctopusPatternViewController.swift
+++ Crochet Octopus Counter/OctopusPatternViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class Round {
+class RoundGroup {
     var text:String
-    var totalStitches:Int
-    init(text:String, totalStitches:Int) {
+    var stitchesPerRound:Int
+    var numRounds:Int
+    
+    init(text:String, stitchesPerRound:Int, numRounds:Int) {
         self.text = text
-        self.totalStitches = totalStitches
+        self.stitchesPerRound = stitchesPerRound
+        self.numRounds = numRounds
     }
+    
+    var roundsCompleted:Int = 0
 }
 
 class OctopusPatternViewController: UIViewController {
@@ -29,50 +34,65 @@ class OctopusPatternViewController: UIViewController {
     
     var counter = 0
     var currentRow = 0
-    var rounds:[Round] = []
+    var roundGroups:[RoundGroup] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.estimatedRowHeight = 66.0
+        tableView.rowHeight = UITableViewAutomaticDimension
         
         tableView.selectRow(at: IndexPath(row: currentRow, section: 0), animated: false, scrollPosition: .none)
         
         tableViewHeightConstraint.constant = (view.frame.size.height - frogButton.frame.size.height)/2
         
-        rounds.append(Round(text: "Rnd 1: 6 sc in Magic Ring, mark beginning of each round with stitch marker (6 sts)", totalStitches: 2))
-        rounds.append(Round(text: "2sc in each sc around (12 sts)", totalStitches: 2))
-        rounds.append(Round(text: "*1sc in next sc, 2sc in next sc; rep from *, 6 times (18 sts)", totalStitches: 4))
-        rounds.append(Round(text: "*1sc in next 2 sc, 2sc in next sc; rep from *, 6 times (24 sts)", totalStitches: 4))
-        rounds.append(Round(text: "*1sc in next 3 sc, 2sc in next sc; rep from *, 6 times (30 sts)", totalStitches: 6))
-        rounds.append(Round(text: "*1sc in next 4 sc, 2sc in next sc; rep from *, 6 times (36 sts)", totalStitches: 6))
-        rounds.append(Round(text: "Rnds 7-14: Sc in each single crochet around (36 sts)", totalStitches: 6))
-        rounds.append(Round(text: "Rnd 15: *1sc in next 4 sc, sc2tog; rep from *, 6 times (30 sts)", totalStitches: 6))
-        rounds.append(Round(text: "Rnds 16-17: 1sc in each single crochet around (30 sts)", totalStitches: 6))
-        rounds.append(Round(text: "Rnd 18: *1sc in next 3 sc, sc2tog; rep from *, 6 times (24 sts)", totalStitches: 6))
-        rounds.append(Round(text: "Rnds 19-20: 1 sc in each single crochet around (24 sts)", totalStitches: 6))
-        rounds.append(Round(text: "Rnd 21: *1sc in next 2 sc, sc2tog; rep from *, 6 times (18 sts)", totalStitches: 6))
-        rounds.append(Round(text: "Rnd 22: 1sc in each single crochet around (18sts)", totalStitches: 6))
-        rounds.append(Round(text: "Rnd 23: *1sc in next 7 single crochet, sc2tog; rep from *, 2 times (16 sts)", totalStitches: 6))
-        rounds.append(Round(text: "Rnd 24: 1sc in each single crochet around (16 sts)", totalStitches: 6))
+        roundGroups.append(RoundGroup(text: "Rnd 1: 6 sc in Magic Ring, mark beginning of each round with stitch marker (6 sts)", stitchesPerRound: 2, numRounds: 1))
+        roundGroups.append(RoundGroup(text: "2sc in each sc around (12 sts)", stitchesPerRound: 2, numRounds: 1))
+        roundGroups.append(RoundGroup(text: "*1sc in next sc, 2sc in next sc; rep from *, 6 times (18 sts)", stitchesPerRound: 4, numRounds: 3))
+        roundGroups.append(RoundGroup(text: "*1sc in next 2 sc, 2sc in next sc; rep from *, 6 times (24 sts)", stitchesPerRound: 4, numRounds: 1))
+        roundGroups.append(RoundGroup(text: "*1sc in next 3 sc, 2sc in next sc; rep from *, 6 times (30 sts)", stitchesPerRound: 6, numRounds: 1))
+        roundGroups.append(RoundGroup(text: "*1sc in next 4 sc, 2sc in next sc; rep from *, 6 times (36 sts)", stitchesPerRound: 6, numRounds: 1))
+        roundGroups.append(RoundGroup(text: "Rnds 7-14: Sc in each single crochet around (36 sts)", stitchesPerRound: 6, numRounds: 7))
+        roundGroups.append(RoundGroup(text: "Rnd 15: *1sc in next 4 sc, sc2tog; rep from *, 6 times (30 sts)", stitchesPerRound: 6, numRounds: 1))
+        roundGroups.append(RoundGroup(text: "Rnds 16-17: 1sc in each single crochet around (30 sts)", stitchesPerRound: 6, numRounds: 1))
+        roundGroups.append(RoundGroup(text: "Rnd 18: *1sc in next 3 sc, sc2tog; rep from *, 6 times (24 sts)", stitchesPerRound: 6, numRounds: 1))
+        roundGroups.append(RoundGroup(text: "Rnds 19-20: 1 sc in each single crochet around (24 sts)", stitchesPerRound: 6, numRounds: 1))
+        roundGroups.append(RoundGroup(text: "Rnd 21: *1sc in next 2 sc, sc2tog; rep from *, 6 times (18 sts)", stitchesPerRound: 6, numRounds: 1))
+        roundGroups.append(RoundGroup(text: "Rnd 22: 1sc in each single crochet around (18sts)", stitchesPerRound: 6, numRounds: 1))
+        roundGroups.append(RoundGroup(text: "Rnd 23: *1sc in next 7 single crochet, sc2tog; rep from *, 2 times (16 sts)", stitchesPerRound: 6, numRounds: 1))
+        roundGroups.append(RoundGroup(text: "Rnd 24: 1sc in each single crochet around (16 sts)", stitchesPerRound: 6, numRounds: 1))
         
         counterButtonLabel.setTitle("0", for: .normal)
         
         frogButton.imageView?.contentMode = .scaleAspectFit
-        
     }
     
     @IBAction func counterButtonTapped(_ sender: UIButton) {
         counter += 1
-                
-        if counter == rounds[currentRow].totalStitches {
-            currentRow += 1
-            counter = 0
+        
+        if counter == roundGroups[currentRow].stitchesPerRound {
             
+            if roundGroups[currentRow].numRounds == 3 {
+                if counter == roundGroups[currentRow].stitchesPerRound {
+                    counter = 0
+                    roundGroups[currentRow].roundsCompleted += 1
+                }
+                
+                if roundGroups[currentRow].roundsCompleted == 4 {
+                    currentRow += 1
+                }
+            }
+                
+            else {
+                currentRow += 1
+            }
+            
+            counter = 0
             tableView.selectRow(at: IndexPath(row: currentRow, section: 0), animated: false, scrollPosition: .none)
         }
         
         counterButtonLabel.setTitle("\(counter)", for: .normal)
         frogButton.isUserInteractionEnabled = true
-        
     }
     
     @IBAction func frogButtonTapped(_ sender: UIButton) {
@@ -83,27 +103,22 @@ class OctopusPatternViewController: UIViewController {
         if counter == 0 {
             frogButton.isUserInteractionEnabled = false
         }
-        
     }
-    
-    
 }
-
 
 extension OctopusPatternViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return rounds.count
+        return roundGroups.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PatternRowTableViewCell", for: indexPath) as! PatternRowTableViewCell
         
-        cell.rowPatternLabel.text = rounds[indexPath.row].text
+        cell.rowPatternLabel.text = roundGroups[indexPath.row].text
         
         return cell
     }
 }
-
 
 extension OctopusPatternViewController: UITableViewDelegate {
     
